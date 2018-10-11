4d48326401cfd9da3bce4a1dc219e9ae3f668955
otaree <deep.yen.ur.hash@gmail.com>
2018-10-11 11:04:26 +0530

    add support for responsive menu

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,13 +5,13 @@
           <a @click="navigateTo('home')" class="navbar-item">
             <img src="../assets/images/dina.png" alt="Logo">
           </a>
-          <span class="navbar-burger burger" data-target="navbarMenuHeroC">
+          <span class="navbar-burger burger" data-target="navbarMenuHeroC" @click="toggleMenu">
             <span></span>
             <span></span>
             <span></span>
           </span>
         </div>
-        <div class="navbar-menu">
+        <div class="navbar-menu" :class="{ 'is-active': isSnackBarActive }">
           <div class="navbar-end">
             <a class="navbar-item" @click="navigateTo('home')"  :class="homeActive" >
               Home
@@ -35,11 +35,13 @@
 export default {
   data() {
     return {
-      active: "home"
+      active: "home",
+      isSnackBarActive: false
     };
   },
   computed: {
     homeActive() {
+      this.isSnackBarActive = false
       return { "is-active": this.active === "home" };
     },
     aboutActive() {
@@ -56,6 +58,9 @@ export default {
     navigateTo(name) {
       this.active = name;
       this.$router.push({ name });
+    },
+    toggleMenu () {
+      this.isSnackBarActive = !this.isSnackBarActive
     }
   },
   mounted() {
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="container">
-    <h1 class="title has-text-centered" data-aos="fade-right">Contact</h1>
+    <div class="content">
+      <h1 class="title has-text-centered" data-aos="fade-right">Contact</h1>
     <hr data-aos="fade-left">
     <h2 class="subtitle has-text-centered">Let’s get in touch!</h2>
     <p class="has-text-centered">You can send me an email at:</p>
-    <p data-aos="flip-left" data-aos-duration="3000" class="is-size-3 has-text-centered has-text-info has-text-weight-light">dinabandhu.chakraborty.jk@gmail.com</p>
+    <p data-aos="flip-left" data-aos-duration="3000" class="is-size-3 is-size-6-mobile has-text-centered has-text-info has-text-weight-light">dinabandhu.chakraborty.jk@gmail.com</p>
+    </div>
   </div>
 </template>
 
